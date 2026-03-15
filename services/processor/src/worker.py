4d0949f65ff279c76033b26c8e3778e6c4ba9cfc
oQuintino <matheus.quintinodasilva0@gmail.com>
2026-03-15 15:20:30 -0300

    refactor: switch processor to rq worker and remove main.py

---
+++ services/processor/src/worker.py
@@ -0,0 +1,14 @@
+import os
+
+from redis import Redis
+from rq import Worker
+
+redis_url = os.getenv("REDIS_URL")
+
+conn = Redis.from_url(redis_url)
+
+if __name__ == "__main__":
+    print("RQ worker iniciado")
+
+    worker = Worker(("emission_queue",), connection=conn)
+    worker.work()
